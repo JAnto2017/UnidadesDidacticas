d6df18222476e601a74b87c31b552979b1e85775
JAnto2017 <cursooperaciones2017@gmail.com>
2020-01-30 21:30:36 +0000

    primer commit

---
+++ app/src/main/java/com/example/unidadesdidacticas/PropuestasActividades.java
@@ -0,0 +1,14 @@
+package com.example.unidadesdidacticas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PropuestasActividades extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_propuestas_actividades);
+    }
+}
